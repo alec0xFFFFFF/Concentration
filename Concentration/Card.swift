414489d46984cb844ad18615219cfb59757a0ba8
aleckwhite <alexander.k.white@gmail.com>
2018-07-30 23:29:41 -0400

    base commit

---
+++ Concentration/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Alexander K. White on 7/30/18.
+//  Copyright © 2018 Alexander K. White. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+}
