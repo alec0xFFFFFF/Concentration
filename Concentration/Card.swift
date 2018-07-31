4ecbd3bd8f0afa84224f4a3a867cc602a918a97d
aleckwhite <alexander.k.white@gmail.com>
2018-07-31 00:30:15 -0400

    implemented models and view controller for game

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -10,4 +10,18 @@ import Foundation
 
 struct Card {
     
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identityFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identityFactory += 1
+        return identityFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
 }
--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,4 +10,37 @@ import Foundation
 
 class Concentration {
     
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index:Int) {
+        if !cards[index].isMatched{
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //  check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil //  no longer one and only...
+            } else {
+                //  either two cards or none face up
+                for flipdownIndex in cards.indices {
+                    cards[flipdownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairOfCards:Int) {
+        for _ in 1...numberOfPairOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        //  TODO: shuffle cards
+    }
+    
 }
