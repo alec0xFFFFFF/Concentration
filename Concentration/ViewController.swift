9e0b269be36c8cb8ace669917f2ae9d183aa1321
aleckwhite <alexander.k.white@gmail.com>
2018-07-31 23:52:38 -0400

    connected storyboard to view controller

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.2459914088, blue: 0.04210116714, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
             
         }
